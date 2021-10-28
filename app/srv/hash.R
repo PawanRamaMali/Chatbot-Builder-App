3fcad87d37f927af3f2dba28e403f8fd4913c612
Pawan Rama Mali <prm@outlook.in>
2021-10-28 23:25:31 +0530

    Build

--- app/srv/hash.R
+++ app/srv/hash.R
@@ -1,11 +1,2 @@
 
-find_hashed_output <- eventReactive(input$start_hash, {
-  
-  digest::digest(input$text_to_hash,
-                 algo = input$select_hash,
-                 serialize = FALSE)
-})
 
-output$hashed_output <- renderText({
-  find_hashed_output()
-})
--- app/ui/admin/build_page.R
+++ app/ui/admin/build_page.R
@@ -4,50 +4,28 @@ tabPanel("Build",
          value = "Build",
          fluidPage
          (fluidRow(
-           h3("Build you Hashed and Salted Password"),
+           h3("Build your Chatbot"),
            
-           column(
-             2,
-             selectInput(
-               inputId = "select_hash",
-               label = "Select Hash Function",
-               choices = c(
-                 "md5",
-                 "sha1",
-                 "crc32",
-                 "sha256",
-                 "sha512",
-                 "xxhash32",
-                 "xxhash64",
-                 "murmur32",
-                 "spookyhash",
-                 "blake3"
-               ),
-               selected = "sha512",
-               multiple = FALSE
-             ),
-           ),
+           br(),
+           br(),
+          
            
            column(
              2,
              textInput(
-               inputId = "text_to_hash",
+               inputId = "Chatbot_Name",
                value = "",
-               label = "Enter Password to hash : "
+               label = "Enter your Chatbot name : "
              ),
              
              actionButton(
-               inputId = "start_hash",
+               inputId = "start_build",
                label = "Submit",
                icon = NULL,
                width = "100%",
                class = "btn-success"
              )
-           ),
-           column(3,
-                  fluidRow(
-                    verbatimTextOutput(outputId = "hashed_output",
-                                       placeholder = FALSE)
-                  ))
+           )
+          
            
          )))
