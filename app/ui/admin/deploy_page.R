b76c61f3fc3598ecb32d14d925e566d0430aaff3
Pawan Rama Mali <prm@outlook.in>
2021-10-30 16:26:00 +0530

    Create deploy_page.R

---
+++ app/ui/admin/deploy_page.R
@@ -0,0 +1,13 @@
+##  Deploy Case ----
+
+tabPanel("Deploy",
+         value = "Deploy",
+         fluidPage
+         (fluidRow(h3("Deploy chatbot")),
+           
+           
+           br(),
+           br()
+           
+           
+         ))
